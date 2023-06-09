0b8a289a86ef1a12b53a921c71d469ebd3d62e53
Vil <101118033+villeneuveandhira@users.noreply.github.com>
2023-06-09 13:47:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* import(s) */
 import React from "react";
 import Home from './components/Home';
+// import Penerbit from './components/penerbit/Penerbit';
 //import {Container} from "react-bootstrap"
 //import React, { useState, useEffect } from 'react';
 //import axios from 'axios';
@@ -10,6 +11,7 @@ function App() {
   return (
     <div>
       <Home />
+      {/* <Penerbit /> */}
     </div>
   );
 }
